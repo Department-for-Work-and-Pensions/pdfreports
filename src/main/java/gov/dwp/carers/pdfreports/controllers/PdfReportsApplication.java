9f09a4d30a8a3c41da871e7c1c864ddb8e3fff93
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-10-24 23:07:04 +0100

    CADS-1406 : Split out reports from PdfRenderer. Remove springboot but keeping spring-boot-starter-test.

--- src/main/java/gov/dwp/carers/pdfreports/controllers/PdfReportsApplication.java
+++
@@ -1,7 +0,0 @@
-package gov.dwp.carers.pdfreports.controllers;
-
-public class PdfReportsApplication {
-    public static void main(final String... args) throws Exception {
-        System.out.println("DUMMY MAIN NO FUNCTION");
-    }
-}
